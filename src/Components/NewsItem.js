307499e28fd6bfe9c0c7ebf0bb1ca30429856c58
Pranav Modgil <pranavmodgil@gmail.com>
2023-06-30 22:01:51 +0530

    Added state in newsItem component

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-        
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-        <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/3638/production/_130208831_gettyimages-1016089960.jpg" className="card-img-top" alt="..."/>
+        <img src={imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail" className="btn btm-sm btn-primary">Read More</a>
         </div>
         </div>
       </div>
