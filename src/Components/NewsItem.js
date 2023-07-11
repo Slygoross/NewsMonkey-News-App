66d4356439f931d32f2599b24b71f929948d9ae8
Pranav Modgil <pranavmodgil@gmail.com>
2023-07-11 12:22:09 +0530

    Added Live news fetching using fetch API

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,10 +6,10 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <img src={!imageUrl?"https://c.ndtvimg.com/2023-07/tj60sh4o_cereal_625x300_10_July_23.jpg?im=FaceCrop,algorithm=dnn,width=1200,height=675":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a href={newsUrl} target="-blank" className="btn btm-sm btn-primary">Read More</a>
         </div>
         </div>
